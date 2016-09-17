b2a753250b7c1eba557362362209b8cdad4833b8
doccfanlee <doccfanlee@yahoo.com>
2016-09-17 18:01:47 +0800

    Exact Change added

--- AdvancedAlgorithmScripting/Exact Change.js
+++ AdvancedAlgorithmScripting/Exact Change.js
@@ -1,4 +1,3 @@
-
 function checkCashRegister(price, cash, cid) {
     console.log("New test:");
     var change = cash - price;
@@ -84,18 +83,3 @@ function Change(unitAlpha, left) {
         }
     };
 }
-$(document).ready(function () {
-    // Example cash-in-drawer array:
-    // [["PENNY", 1.01],
-    // ["NICKEL", 2.05],
-    // ["DIME", 3.10],
-    // ["QUARTER", 4.25],
-    // ["ONE", 90.00],
-    // ["FIVE", 55.00],
-    // ["TEN", 20.00],
-    // ["TWENTY", 60.00],
-    // ["ONE HUNDRED", 100.00]]
-    console.log(
-        checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])
-    );
-});
