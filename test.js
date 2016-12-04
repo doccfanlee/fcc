661eff42e8580a0a70297003b4584afb91338761
doccfanlee <doccfanlee@yahoo.com>
2016-12-04 14:34:46 +0800

    update with scss

--- test.js
+++ test.js
@@ -1,37 +1,34 @@
-var Person = function(firstAndLast) {
-    var firstName = firstAndLast.split(" ")[0];
-    var lastName = firstAndLast.split(" ")[1];
-    
-    this.getFirstName = function(){
-      return this.firstName;
-    };
-    
-    this.getLastName = function() {
-      return this.lastName;
-    };
-    
-    this.getFullName = function() {
-      return this.firstName + " " + this.lastName;
-    };
-  
-      this.setFirstName = function(fName){
-       this.firstName = fName;
-    };
-    
-    this.setLastName = function(lName) {
-       this.lastName = lName;
-    };
-    
-    this.setFullName = function(fullName) {
-      this.firstName = fullName.split(" ")[0];
-      this.lastName = fullName.split(" ")[1];
-      
-    };
-    
-};
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+  var retArr = [];
+    arr2.forEach(function(val){
+      var tempArr = [];
+      tempArr = arr1.filter(function(element){
+        return element[1] === val[1];
+      });
+      if(tempArr === []){
+        retArr.push(val);
+      } else {
+        retArr.push([tempArr[0]+val[0], val[1]]);
+      }
 
-var bob = new Person('Bob Ross');
-bob.getFullName();
-bob.getFirstName();
-bob.setLastName("hao");
-bob.setFullName("Li Hao");
+    });
+    return retArr;
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
