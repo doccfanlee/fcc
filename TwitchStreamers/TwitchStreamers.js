c15c13e38dd77e83f9599ecacb04ba9e4869926b
doccfanlee <doccfanlee@yahoo.com>
2016-12-08 12:45:42 +0800

    Version 1.0.0.0

--- TwitchStreamers/TwitchStreamers.js
+++ TwitchStreamers/TwitchStreamers.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var users_TwitchStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     $("#tag_online").on("mouseenter", function () {
         $(this).animate({
             "margin-left": "0px",
@@ -12,6 +13,10 @@ $(document).ready(function () {
         $(this).animate({
             "margin-left": "95px",
         }, 500);
+    });
+    $("#tag_online").on("click", function () {
+        $(".offline").hide();
+        $(".online").show();
     });
 
     $("#tag_offline").on("mouseenter", function () {
@@ -28,22 +33,74 @@ $(document).ready(function () {
             "margin-left": "95px",
         }, 500);
     });
+    $("#tag_offline").on("click", function () {
+        $(".online").hide();
+        $(".offline").show();
+    });
+    $("#tag_all").on("click", function () {
+        $(".online").show();
+        $(".offline").show();
+    });
 
-    getInfo();
+    //var json = {};
+    users_TwitchStreamers.forEach(function (element) {
+        //getUserInfo(element);
+        // console.log("infoFromTwitchTV is " + json);
+
+        var channelUrl = "https://wind-bow.gomix.me/twitch-api/channels/" + element + "?callback=?";
+        var streamlUrl = "https://wind-bow.gomix.me/twitch-api/streams/" + element + "?callback=?";
+        var dataGet = {};
+        dataGet.Accept = 'application/vnd.twitchtv.v3+json';
+        $.ajax({
+            method: "GET",
+            url: channelUrl,
+            dataType: "jsonp",
+            data: dataGet,
+        }).done(function (channelData) {
+            $.ajax({
+                method: "GET",
+                url: streamlUrl,
+                dataType: "jsonp",
+                data: dataGet,
+            }).done(function (streamData) {
+                var jsonStatus = "";
+                if (streamData.stream == null) {
+                    jsonStatus = "offline";
+                } else {
+                    jsonStatus = streamData.game;
+                    if (jsonStatus.length > 25)
+                        jsonStatus = streamData.game.slice(0, 25) + "...";
+                }
+                console.log(streamData);
+                console.log(channelData);
+                var onOrOffline = jsonStatus === "offline" ? ' offline' : ' online';
+                if (channelData !== undefined) {
+                    var myNewElement = $('<div class="row' + onOrOffline + '">\
+                        <div class="col-xs-1">\
+                            <img class="item_image" src="'+ channelData.logo + '" alt="pic" >\
+                        </div>\
+                        <div class="col-xs-11">\
+                            <div class="row">\
+                                <div class="col-sm-4">\
+                                    <p class="text-center item_name"><a href="'+ channelData.url + '">' + channelData.display_name + '</a></p>\
+                                </div>\
+                                <div class="col-sm-8">\
+                                    <p class="text-center item_status">'+ jsonStatus + '</p>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>' );
+                }
+                myNewElement.appendTo("#TwitchStreamersList");
+            }).fail(function () {
+                console.log("failed.");
+            });
+
+        }).fail(function () {
+            console.log("failed.");
+        });
+
+    });
+
 });
 
-function getInfo() {
-    var dataGet = {};
-    dataGet.Accept = 'application/vnd.twitchtv.v3+json';
-    $.ajax({
-        method: "GET",
-        url: "https://api.twitch.tv/kraken/channels/test_channel",
-        dataType: "jsonp",
-        data: dataGet,
-    }).done(function (json) {
-        console.log(json);
-
-    }).fail(function () {
-        console.log("failed.");
-    });
-}
