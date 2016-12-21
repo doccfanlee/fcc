8c71077e5ea154d68bc03280c07582cae3b16a20
doccfanlee <doccfanlee@yahoo.com>
2016-12-21 14:15:23 +0800

    Add .gitignore

--- SimonGame/SimonGame.js
+++ SimonGame/SimonGame.js
@@ -1,5 +1,21 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
+    var AIAudioPlayOrder = [];
+    var PlayerAudioPlayOrder = [];
+    $("#redBtnId").on("click", function () {
+        PlayAudio("#audio1");
+    });
+    $("#greenBtnId").on("click", function () {
+        PlayAudio("#audio2");
+    });
+    $("#blueBtnId").on("click", function () {
+        PlayAudio("#audio3");
+    });
+    $("#yellowBtnId").on("click", function () {
+        PlayAudio("#audio4");
+    });
 
-
+    function PlayAudio(audioId){
+        $(audioId)[0].play();
+    }
 });
