38c7a44b8d8f481b329ca03c7d242057d823b552
LiYanhao <l_hao@aol.com>
2017-10-22 19:41:11 +0800

    Unknown commit

--- test.js
+++ test.js
@@ -1,37 +1,63 @@
-var Person = function(firstAndLast) {
-    var firstName = firstAndLast.split(" ")[0];
-    var lastName = firstAndLast.split(" ")[1];
-    
-    this.getFirstName = function(){
-      return this.firstName;
-    };
-    
-    this.getLastName = function() {
-      return this.lastName;
-    };
-    
-    this.getFullName = function() {
-      return this.firstName + " " + this.lastName;
-    };
-  
-      this.setFirstName = function(fName){
-       this.firstName = fName;
-    };
-    
-    this.setLastName = function(lName) {
-       this.lastName = lName;
-    };
-    
-    this.setFullName = function(fullName) {
-      this.firstName = fullName.split(" ")[0];
-      this.lastName = fullName.split(" ")[1];
-      
-    };
-    
-};
+function updateInventory(arr1, arr2) {
+  // All inventory must be accounted for or you're fired!
+  var retArr = arr1;
 
-var bob = new Person('Bob Ross');
-bob.getFullName();
-bob.getFirstName();
-bob.setLastName("hao");
-bob.setFullName("Li Hao");
+  arr2.forEach(function (arr2Val, index) {
+    var arr1Id = arr1.findIndex(function (arr1Val) {
+      return arr2Val[1] === arr1Val[1];
+    });
+    if (arr1Id < 0) {
+      retArr.push(arr2Val);
+    } else {
+      retArr[arr1Id] = [arr1[arr1Id][0] + arr2Val[0], arr1[arr1Id][1]];
+    }
+  });
+  /* another way to update inventory
+    for(var i = 0; i < arr2.length; i++){
+      for(var j = 0; j < arr1.length; j++){
+        if(arr2[i][1] === arr1[j][1]){
+          retArr[j][0] = arr2[i][0] + arr1[j][0];
+          break;
+        } else {
+          if(j === arr1.length - 1){
+            retArr.push(arr2[i]);
+            break;
+          }
+        }
+      }
+    }
+    */
+
+  retArr.sort(function (a, b) {
+    var nameA = a[1].toUpperCase(); // ignore upper and lowercase
+    var nameB = b[1].toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+
+  return retArr;
+}
+
+// Example inventory lists
+var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"]
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
